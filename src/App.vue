91ee47e5fce0c01e9ef587e029d85a9ef31336cb
talissonengelmann <talisson.engelmann@gmail.com>
2026-06-17 19:58:59 -0300

    Atualiza projeto para deploy

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import Banner from "../src/components/Banner.vue";
+import ConteudoPrincipal from "./components/ConteudoPrincipal.vue";
+import Rodape from "./components/Rodape.vue";
+
+export default {
+  components: {
+    Banner,
+    ConteudoPrincipal,
+    Rodape
+  }
+}
+</script>
+
+<template>
+    <Banner />
+    <ConteudoPrincipal />
+    <Rodape />
+</template>
